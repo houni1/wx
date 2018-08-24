9779d42ff36acc5d421a1be10bff42b976bf1508
Edianzu <chengkunkun9018-pq@emao.com>
2018-08-24 22:07:20 +0800

    '在售车型页面布局'

---
+++ pages/cart/onSale/onSale.js
@@ -0,0 +1,90 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabIndex: 0,        // 顶部tab切换索引
+    showSelect: false   // 全部商品筛选
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+  /**
+   * 页面顶部tab切换
+   */
+  filterTabChange: function (event) {
+    var index = event.currentTarget.dataset.index;
+    if (index == 2 && !this.data.showSelect) {
+      console.log(123)
+      this.setData({
+        showSelect: true
+      });
+    } else {
+      this.setData({
+        showSelect: false
+      });
+    }
+    this.setData({
+      tabIndex: index
+    });
+  },
+  /**
+   * 全部商品下拉框显示隐藏
+   */
+  goodsSelect: function (event){
+    console.log(event.currentTarget.dataset.kind);
+    this.setData({
+      showSelect: false
+    });
+  },
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
