d82e1623643ca816a3aac1abaae968c3fdcb2343
houluyao9001-pq@emao.com <houluyao9001-pq@emao.com>
2018-08-30 14:49:39 +0800

    上传图片方法测试

--- pages/cart/setup/setup.js
+++ pages/cart/setup/setup.js
@@ -1,4 +1,5 @@
 // pages/cart/setup/setup.js
+// import { uploadImage } from '../../../utils/upload.js';
 Page({
 
   /**
@@ -64,47 +65,8 @@ Page({
   
   },
 
-  onloadImage: function () {
-    let _this = this
-    wx.showActionSheet({
-      itemList: ['从相册中选择', '拍照'],
-      itemColor: "#f7982a",
-      success: function (res) {
-        if (!res.cancel) {
-          if (res.tapIndex == 0) {
-            _this.addImage('album')
-          } else if (res.tapIndex == 1) {
-            _this.addImage('camera')
-          }
-        }
-      }
-    })
-  },
-
-  // 上传图片
-  addImage: function (types) {
-    var _this = this;
-    wx.chooseImage({
-      count: 2, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: [types], // 可以指定来源是相册还是相机，默认二者都有
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        // console.log(res.tempFilePaths[0])
-        _this.urlTobase64(res.tempFilePaths[0])
-      }
-    })
-  },
-
-  urlTobase64: function (url) {
-    wx.request({
-      url: url,
-      responseType: 'arraybuffer', //最关键的参数，设置返回的数据格式为arraybuffer
-      success: res => {
-        let base64 = wx.arrayBufferToBase64(res.data); //把arraybuffer转成base64
-        base64 　= 'data:image/jpeg;base64,' + base64　//不加上这串字符，在页面无法显示的哦
-        console.log(base64)　//打印出base64字符串，可复制到网页校验一下是否是你选择的原图片呢
-      }
-    })
+  uploadImage: function () {
+    console.log('添加图片')
   }
+
 })
