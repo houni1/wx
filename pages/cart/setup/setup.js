ec580fff1e4d21341e92c828ba7ee4781b3f4221
houluyao9001-pq@emao.com <houluyao9001-pq@emao.com>
2018-09-03 10:49:39 +0800

    上传图片等功能

--- pages/cart/setup/setup.js
+++ pages/cart/setup/setup.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imageList: []
+    imageList: [],
+    imageArr: []
   },
 
   /**
@@ -65,8 +66,96 @@ Page({
   
   },
 
+  // 点击添加按钮弹起选择类型框
   uploadImage: function () {
-    console.log('添加图片')
+    let _this = this
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#f7982a",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            _this.addImage('album')
+          } else if (res.tapIndex == 1) {
+            _this.addImage('camera')
+          }
+        }
+      }
+    })
+  },
+
+  // 选择图片，获取图片信息
+  addImage: function (types) {
+    var _this = this;
+    wx.chooseImage({
+      count: 6, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: [types], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        _this.setData({
+          imageList: _this.data.imageList.concat(res.tempFilePaths)
+        })
+        if (_this.data.imageList.length > 6) {
+          wx.showToast({
+            icon: 'none',
+            title: '您只能添加6张图'
+          })
+          _this.setData({
+            imageList: _this.data.imageList.slice(0, 6)
+          })
+        }
+      }
+    })
+  },
+
+  // 删除图片
+  closeimg: function (e) {
+    var ind = e.currentTarget.dataset.ind
+    console.log(ind)
+
+    this.data.imageList.splice(ind, 1)
+
+    this.setData({
+      imageList: this.data.imageList
+    })
+    // console.log(this.data.imageList)
+  },
+
+  // 点击预览图片
+  lookpic: function (e) {
+    var imgurl = e.currentTarget.dataset.imgurl
+    console.log(imgurl)
+    console.log(this.data.imageList)
+    wx.previewImage({
+      current: imgurl, // 当前显示图片的http链接
+      urls: this.data.imageList // 需要预览的图片http链接列表
+    })
+  },
+
+
+  // 提交表单提交页面
+  uploadfile: function () {
+    console.log(this.data.imageList)
+    this.setData({
+      imageArr: this.data.imageList.join(',')
+    })
+    console.log(this.data.imageArr)
+    wx.uploadFile({
+      url: 'https://tcmapi.emao.com/lottery/imgUpload', //仅为示例，非真实的接口地址  
+      filePath: this.data.imageArr,
+      name: 'file',
+      header: {
+        "X-Emao-TCM-App": "os=Android 7.1.1;model=Xiaomi MIX2;appVersion=2.1.0",
+        'Accept': 'application/json; version=3.8.0'
+      },
+      formData: {
+        userid: '1'
+      },
+      success: function (res) {
+        var data = res.data
+        console.log(data)
+      }
+    })
   }
 
 })
